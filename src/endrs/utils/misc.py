51ca8abcbb2ec884141b5f0f866139828b59a233
massquantity <wdmjjxg@163.com>
2025-08-14 21:23:45 +0800

    refactor: improve colorize text function

--- src/endrs/utils/misc.py
+++ src/endrs/utils/misc.py
@@ -93,34 +93,31 @@ class LightningProgressBar(TQDMProgressBar):
         pass
 
 
-def colorize(string, color, bold=False, highlight=False):
-    """Return string surrounded by appropriate terminal color codes to
-    print colorized text.  Valid colors: gray, red, green, yellow,
-    blue, magenta, cyan, white, crimson
+def colorize(text: str, color: str, bold: bool = False, highlight: bool = False) -> str:
+    """Apply ANSI color and style formatting to text."""
 
-    Original source from openAI `gym`:
-    https://github.com/openai/gym/blob/master/gym/utils/colorize.py
-    """
+    # Color codes mapping
+    colors = {
+        'gray': 30,
+        'red': 31,
+        'green': 32,
+        'yellow': 33,
+        'blue': 34,
+        'magenta': 35,
+        'cyan': 36,
+        'white': 37,
+        'crimson': 38,
+    }
 
-    attr = []
-    num = color2num[color]
+    attrs = []
+    color_code = colors.get(color.lower(), colors['white'])
     if highlight:
-        num += 10
-    attr.append(str(num))
+        color_code += 10
+
+    attrs.append(str(color_code))
+
     if bold:
-        attr.append("1")
-    attrs = ";".join(attr)
-    return "\x1b[%sm%s\x1b[0m" % (attrs, string)
+        attrs.append("1")
 
-
-color2num = dict(
-    gray=30,
-    red=31,
-    green=32,
-    yellow=33,
-    blue=34,
-    magenta=35,
-    cyan=36,
-    white=37,
-    crimson=38,
-)
+    attrs_string = ";".join(attrs)
+    return f"\x1b[{attrs_string}m{text}\x1b[0m"
