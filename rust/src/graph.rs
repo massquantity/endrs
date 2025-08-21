cec71e8ffb4987659440b00e5b103a68e93bfb8e
massquantity <wdmjjxg@163.com>
2025-08-21 08:30:13 +0800

    feat(rust): add Swing

---
+++ rust/src/graph.rs
@@ -0,0 +1,181 @@
+use std::cmp::Ordering;
+use std::sync::Arc;
+
+use dashmap::DashMap;
+use fxhash::FxHashMap;
+use pyo3::PyResult;
+use rayon::prelude::*;
+use serde::{Deserialize, Serialize};
+
+use crate::sparse::{get_row, CsrMatrix};
+
+type ScoredItems = Vec<(i32, f32)>;
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct Graph {
+    n_users: usize,
+    n_items: usize,
+    alpha: f32,
+    max_cache_num: usize,
+}
+
+impl Graph {
+    pub fn new(n_users: usize, n_items: usize, alpha: f32, max_cache_num: usize) -> Self {
+        Self {
+            n_users,
+            n_items,
+            alpha,
+            max_cache_num,
+        }
+    }
+
+    fn compute_single_swing(
+        &self,
+        target_item: usize,
+        user_interactions: &CsrMatrix<i32, f32>,
+        item_interactions: &CsrMatrix<i32, f32>,
+        prev_scores: &FxHashMap<i32, ScoredItems>,
+        user_weights: &[f32],
+        cached_common_items: Arc<DashMap<usize, Vec<usize>>>,
+    ) -> (i32, ScoredItems) {
+        let target_i32 = target_item as i32;
+        let users = get_row_vec(item_interactions, target_item);
+        if users.len() < 2 {
+            let scores = prev_scores
+                .get(&target_i32)
+                .cloned()
+                .unwrap_or_default();
+            return (target_i32, scores);
+        }
+
+        let mut item_scores = init_item_scores(target_i32, self.n_items, prev_scores);
+        for (j, &u) in users.iter().enumerate() {
+            for &v in &users[(j + 1)..users.len()] {
+                let key = u * self.n_users + v;
+                let common_items = match cached_common_items.get(&key) {
+                    Some(items) => items.to_owned(),
+                    None => {
+                        let items = get_intersect_items(
+                            &get_row_vec(user_interactions, u),
+                            &get_row_vec(user_interactions, v),
+                        );
+                        if cached_common_items.len() < self.max_cache_num {
+                            cached_common_items.insert(key, items.clone());
+                        }
+                        items
+                    }
+                };
+
+                // exclude item self according to the paper
+                let k = (common_items.len() - 1) as f32;
+                let score = user_weights[u] * user_weights[v] * (self.alpha + k).recip();
+                for i in common_items {
+                    if i != target_item {
+                        item_scores[i] += score;
+                    }
+                }
+            }
+        }
+
+        (target_i32, extract_valid_scores(item_scores))
+    }
+
+    pub(crate) fn compute_swing_scores(
+        &self,
+        user_interactions: &CsrMatrix<i32, f32>,
+        item_interactions: &CsrMatrix<i32, f32>,
+        prev_mapping: &FxHashMap<i32, ScoredItems>,
+    ) -> PyResult<FxHashMap<i32, ScoredItems>> {
+        let user_weights = compute_user_weights(user_interactions, self.n_users);
+        let cached_common_items = Arc::new(DashMap::new());
+        let swing_scores: Vec<(i32, ScoredItems)> = (1..=self.n_items)
+            .into_par_iter()
+            .map(|i| {
+                self.compute_single_swing(
+                    i,
+                    user_interactions,
+                    item_interactions,
+                    prev_mapping,
+                    &user_weights,
+                    Arc::clone(&cached_common_items),
+                )
+            })
+            .filter(|(_, s)| !s.is_empty())
+            .collect();
+
+        Ok(FxHashMap::from_iter(swing_scores))
+    }
+}
+
+fn compute_user_weights(matrix: &CsrMatrix<i32, f32>, n_users: usize) -> Vec<f32> {
+    let mut user_weights = vec![0.0; n_users + 1];
+    // skip oov 0
+    for (i, weight) in user_weights.iter_mut().enumerate().skip(1) {
+        let n_items = matrix.indptr[i + 1] - matrix.indptr[i];
+        *weight = if n_items == 0 {
+            0.0
+        } else {
+            (n_items as f32).sqrt().recip()
+        };
+    }
+    user_weights
+}
+
+fn get_intersect_items(u_items: &[usize], v_items: &[usize]) -> Vec<usize> {
+    let mut i = 0;
+    let mut j = 0;
+    let mut common_items = Vec::new();
+    while i < u_items.len() && j < v_items.len() {
+        match u_items[i].cmp(&v_items[j]) {
+            Ordering::Less => i += 1,
+            Ordering::Greater => j += 1,
+            Ordering::Equal => {
+                common_items.push(u_items[i]);
+                i += 1;
+                j += 1;
+            }
+        }
+    }
+    common_items
+}
+
+fn get_row_vec(matrix: &CsrMatrix<i32, f32>, n: usize) -> Vec<usize> {
+    if let Some(row) = get_row(matrix, n, false) {
+        row.map(|(i, _)| i as usize).collect()
+    } else {
+        Vec::new()
+    }
+}
+
+fn init_item_scores(
+    target_item: i32,
+    n_items: usize,
+    prev_scores: &FxHashMap<i32, ScoredItems>,
+) -> Vec<f32> {
+    let mut item_scores = vec![0.0; n_items + 1];
+    if let Some(scores) = prev_scores.get(&target_item) {
+        for &(i, s) in scores {
+            item_scores[i as usize] = s;
+        }
+    }
+    item_scores
+}
+
+fn extract_valid_scores(scores: Vec<f32>) -> ScoredItems {
+    let mut non_zero_scores: ScoredItems = scores
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, score)| {
+            if score != 0.0 {
+                Some((i as i32, score))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    if non_zero_scores.len() > 1 {
+        non_zero_scores.sort_unstable_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap().reverse());
+    }
+    non_zero_scores
+}
---
+++ rust/src/swing.rs
@@ -0,0 +1,376 @@
+use fxhash::{FxHashMap, FxHashSet};
+use pyo3::prelude::*;
+use pyo3::types::*;
+use serde::{Deserialize, Serialize};
+
+use crate::graph::Graph;
+use crate::inference::{compute_pred, get_intersect_neighbors, get_rec_items};
+use crate::serialization::{load_model, save_model};
+use crate::sparse::{get_row, CsrMatrix};
+use crate::utils::{create_thread_pool, DEFAULT_PRED};
+
+#[pyclass(module = "endrs_ext", name = "Swing")]
+#[derive(Serialize, Deserialize)]
+pub struct PySwing {
+    top_k: usize,
+    alpha: f32,
+    max_cache_num: usize,
+    n_users: usize,
+    n_items: usize,
+    graph: Graph,
+    swing_score_mapping: FxHashMap<i32, Vec<(i32, f32)>>,
+    user_interactions: CsrMatrix<i32, f32>,
+    item_interactions: CsrMatrix<i32, f32>,
+    user_consumed: FxHashMap<i32, Vec<i32>>,
+}
+
+#[pymethods]
+impl PySwing {
+    #[setter]
+    fn set_n_users(&mut self, n_users: usize) {
+        self.n_users = n_users;
+    }
+
+    #[setter]
+    fn set_n_items(&mut self, n_items: usize) {
+        self.n_items = n_items;
+    }
+
+    #[setter]
+    fn set_user_consumed(&mut self, user_consumed: &Bound<'_, PyDict>) -> PyResult<()> {
+        self.user_consumed = user_consumed.extract::<FxHashMap<i32, Vec<i32>>>()?;
+        Ok(())
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    #[new]
+    fn new(
+        top_k: usize,
+        alpha: f32,
+        max_cache_num: usize,
+        n_users: usize,
+        n_items: usize,
+        user_interactions: &Bound<'_, PyAny>,
+        item_interactions: &Bound<'_, PyAny>,
+        user_consumed: &Bound<'_, PyDict>,
+    ) -> PyResult<Self> {
+        let user_consumed: FxHashMap<i32, Vec<i32>> = user_consumed.extract()?;
+        let user_interactions: CsrMatrix<i32, f32> = user_interactions.extract()?;
+        let item_interactions: CsrMatrix<i32, f32> = item_interactions.extract()?;
+        let graph = Graph::new(n_users, n_items, alpha, max_cache_num);
+
+        Ok(Self {
+            top_k,
+            alpha,
+            max_cache_num,
+            n_users,
+            n_items,
+            graph,
+            swing_score_mapping: FxHashMap::default(),
+            user_interactions,
+            item_interactions,
+            user_consumed,
+        })
+    }
+
+    fn compute_swing(&mut self, num_threads: usize) -> PyResult<()> {
+        let pool = create_thread_pool(num_threads)?;
+        self.swing_score_mapping.clear();
+
+        self.swing_score_mapping = pool.install(|| {
+            self.graph.compute_swing_scores(
+                &self.user_interactions,
+                &self.item_interactions,
+                &self.swing_score_mapping,
+            )
+        })?;
+        Ok(())
+    }
+
+    /// update on new sparse interactions
+    fn update_swing(
+        &mut self,
+        num_threads: usize,
+        user_interactions: &Bound<'_, PyAny>,
+        item_interactions: &Bound<'_, PyAny>,
+    ) -> PyResult<()> {
+        let pool = create_thread_pool(num_threads)?;
+        let new_user_interactions: CsrMatrix<i32, f32> = user_interactions.extract()?;
+        let new_item_interactions: CsrMatrix<i32, f32> = item_interactions.extract()?;
+
+        self.swing_score_mapping = pool.install(|| {
+            self.graph.compute_swing_scores(
+                &new_user_interactions,
+                &new_item_interactions,
+                &self.swing_score_mapping,
+            )
+        })?;
+
+        // merge interactions for inference on new users/items
+        self.user_interactions = CsrMatrix::merge(
+            &self.user_interactions,
+            &new_user_interactions,
+            Some(self.n_users),
+        );
+        self.item_interactions = CsrMatrix::merge(
+            &self.item_interactions,
+            &new_item_interactions,
+            Some(self.n_items),
+        );
+        Ok(())
+    }
+
+    fn num_swing_elements(&self) -> PyResult<usize> {
+        if self.swing_score_mapping.is_empty() {
+            return Err(pyo3::exceptions::PyRuntimeError::new_err(
+                "call `compute_swing` method before calling `num_swing_elements`",
+            ));
+        }
+
+        let n_elements = self
+            .swing_score_mapping
+            .values()
+            .map(|i| i.len())
+            .sum();
+
+        Ok(n_elements)
+    }
+
+    fn predict(&self, users: &Bound<'_, PyList>, items: &Bound<'_, PyList>) -> PyResult<Vec<f32>> {
+        let mut preds = Vec::new();
+        let users: Vec<usize> = users.extract()?;
+        let items: Vec<i32> = items.extract()?;
+
+        for (&u, &i) in users.iter().zip(items.iter()) {
+            if u == self.n_users || usize::try_from(i)? == self.n_items {
+                preds.push(DEFAULT_PRED);
+                continue;
+            }
+
+            let pred = match (
+                self.swing_score_mapping.get(&i),
+                get_row(&self.user_interactions, u, false),
+            ) {
+                (Some(item_swings), Some(item_labels)) => {
+                    let num = std::cmp::min(self.top_k, item_swings.len());
+                    let mut item_swing_scores = vec![(0, 0.0); num];
+                    item_swing_scores.clone_from_slice(&item_swings[..num]);
+                    item_swing_scores.sort_unstable_by_key(|&(i, _)| i);
+
+                    let item_labels: Vec<(i32, f32)> = item_labels.collect();
+                    let (k_nb_swings, k_nb_labels) =
+                        get_intersect_neighbors(&item_swing_scores, &item_labels, self.top_k);
+
+                    if k_nb_swings.is_empty() {
+                        DEFAULT_PRED
+                    } else {
+                        compute_pred("ranking", &k_nb_swings, &k_nb_labels)?
+                    }
+                }
+                _ => DEFAULT_PRED,
+            };
+
+            preds.push(pred);
+        }
+
+        Ok(preds)
+    }
+
+    fn recommend<'py>(
+        &self,
+        py: Python<'py>,
+        users: &Bound<'py, PyList>,
+        n_rec: usize,
+        filter_consumed: bool,
+        random_rec: bool,
+    ) -> PyResult<(Vec<Bound<'py, PyList>>, Bound<'py, PyList>)> {
+        let mut recs = Vec::new();
+        let mut additional_rec_counts = Vec::new();
+
+        for u in users {
+            let u: i32 = u.extract()?;
+            let consumed: FxHashSet<i32> = self
+                .user_consumed
+                .get(&u)
+                .map(|v| FxHashSet::from_iter(v.iter().cloned()))
+                .unwrap_or_default();
+
+            let (rec_items, additional_count) =
+                match get_row(&self.user_interactions, usize::try_from(u)?, false) {
+                    Some(row) => {
+                        let mut item_scores: FxHashMap<i32, f32> = FxHashMap::default();
+                        for (i, i_label) in row {
+                            if let Some(item_swings) = self.swing_score_mapping.get(&i) {
+                                let num = std::cmp::min(self.top_k, item_swings.len());
+                                for &(j, i_j_swing_score) in &item_swings[..num] {
+                                    if filter_consumed && consumed.contains(&j) {
+                                        continue;
+                                    }
+
+                                    item_scores
+                                        .entry(j)
+                                        .and_modify(|score| *score += i_j_swing_score * i_label)
+                                        .or_insert(i_j_swing_score * i_label);
+                                }
+                            }
+                        }
+
+                        if item_scores.is_empty() {
+                            (PyList::empty(py), n_rec)
+                        } else {
+                            let items = get_rec_items(item_scores, n_rec, random_rec);
+                            let additional = n_rec - items.len();
+                            (PyList::new(py, items)?, additional)
+                        }
+                    }
+                    None => (PyList::empty(py), n_rec),
+                };
+
+            recs.push(rec_items);
+            additional_rec_counts.push(additional_count);
+        }
+
+        let additional_rec_counts = PyList::new(py, additional_rec_counts)?;
+        Ok((recs, additional_rec_counts))
+    }
+}
+
+#[pyfunction]
+#[pyo3(name = "save_swing")]
+pub fn save(model: &PySwing, path: &str, model_name: &str) -> PyResult<()> {
+    let class_name = "Swing";
+    save_model(model, path, model_name, class_name)?;
+    Ok(())
+}
+
+#[pyfunction]
+#[pyo3(name = "load_swing")]
+pub fn load(path: &str, model_name: &str) -> PyResult<PySwing> {
+    let class_name = "Swing";
+    let model = load_model(path, model_name, class_name)?;
+    Ok(model)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[pyclass]
+    struct PySparseMatrix {
+        #[pyo3(get)]
+        sparse_indices: Vec<i32>,
+        #[pyo3(get)]
+        sparse_indptr: Vec<usize>,
+        #[pyo3(get)]
+        sparse_data: Vec<f32>,
+    }
+
+    fn get_swing_model() -> Result<PySwing, Box<dyn std::error::Error>> {
+        let top_k = 10;
+        let alpha = 1.0;
+        let cache_common_num = 100;
+        let n_users = 3;
+        let n_items = 5;
+        let swing = Python::with_gil(|py| -> PyResult<PySwing> {
+            // item_interactions:
+            // [
+            //     [0, 0, 0, 0],
+            //     [0, 1, 1, 1],
+            //     [0, 1, 1, 0],
+            //     [0, 1, 0, 1],
+            //     [0, 1, 1, 1],
+            //     [0, 0, 0, 1],
+            // ]
+            let item_interactions = Bound::new(
+                py,
+                PySparseMatrix {
+                    sparse_indices: vec![1, 2, 3, 1, 2, 1, 3, 1, 2, 3, 3],
+                    sparse_indptr: vec![0, 0, 3, 5, 7, 10, 11],
+                    sparse_data: vec![1.0; 11],
+                },
+            )?;
+            let user_interactions = Bound::new(
+                py,
+                PySparseMatrix {
+                    sparse_indices: vec![1, 2, 3, 4, 1, 2, 4, 1, 3, 4, 5],
+                    sparse_indptr: vec![0, 0, 4, 7, 11],
+                    sparse_data: vec![1.0; 11],
+                },
+            )?;
+            let user_consumed = [
+                (1, vec![1, 2, 3, 4]),
+                (2, vec![1, 2, 4]),
+                (3, vec![1, 3, 4, 5]),
+            ]
+            .into_py_dict(py)?;
+
+            let mut swing = PySwing::new(
+                top_k,
+                alpha,
+                cache_common_num,
+                n_users,
+                n_items,
+                &user_interactions,
+                &item_interactions,
+                &user_consumed,
+            )?;
+            swing.compute_swing(2)?;
+            Ok(swing)
+        })?;
+        Ok(swing)
+    }
+
+    #[test]
+    fn test_swing_training() -> Result<(), Box<dyn std::error::Error>> {
+        pyo3::prepare_freethreaded_python();
+
+        let test_user_id = 1;
+        let match_item_fn = |model: &PySwing, p: usize, i: i32, s: f32| {
+            let (item, score) = model.swing_score_mapping[&test_user_id][p];
+            item == i && (score - s).abs() < 1e-10
+        };
+
+        let user_weights = [
+            0.0,
+            4_f32.sqrt().recip(),
+            3_f32.sqrt().recip(),
+            4_f32.sqrt().recip(),
+        ];
+        let common_nums = [2.0, 2.0, 1.0]; // user_1_2, user_1_3, user_2_3, k = len(common) - 1;
+        let swing_1_2 = user_weights[1] * user_weights[2] * (1_f32 + common_nums[0]).recip();
+        let swing_1_3 = user_weights[1] * user_weights[3] * (1_f32 + common_nums[1]).recip();
+        let swing_1_4 = user_weights[1] * user_weights[2] * (1_f32 + common_nums[0]).recip()
+            + user_weights[1] * user_weights[3] * (1_f32 + common_nums[1]).recip()
+            + user_weights[2] * user_weights[3] * (1_f32 + common_nums[2]).recip();
+        let swing_model = get_swing_model()?;
+
+        assert_eq!(swing_model.swing_score_mapping[&test_user_id].len(), 3);
+        assert!(match_item_fn(&swing_model, 0, 4, swing_1_4));
+        assert!(match_item_fn(&swing_model, 1, 2, swing_1_2));
+        assert!(match_item_fn(&swing_model, 2, 3, swing_1_3));
+        Ok(())
+    }
+
+    #[test]
+    fn test_save_model() -> Result<(), Box<dyn std::error::Error>> {
+        pyo3::prepare_freethreaded_python();
+
+        let model = get_swing_model()?;
+        let cur_dir = std::env::current_dir()?
+            .to_string_lossy()
+            .to_string();
+        let model_name = "swing_model";
+        save(&model, &cur_dir, model_name)?;
+
+        let new_model: PySwing = load(&cur_dir, model_name)?;
+        Python::with_gil(|py| -> PyResult<()> {
+            let users = PyList::new(py, vec![5, 1])?;
+            let rec_result = new_model.recommend(py, &users, 10, true, false)?;
+            assert_eq!(rec_result.0.len(), 2);
+            Ok(())
+        })?;
+
+        std::fs::remove_file(std::env::current_dir()?.join(format!("{model_name}.gz")))?;
+        Ok(())
+    }
+}
