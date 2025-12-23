e89c6642bdd9293da55ef24dc95d44256991e623
massquantity <wdmjjxg@163.com>
2025-12-23 21:00:14 +0800

    feat(rust): register UserCF, ItemCF, Swing classes in PyO3 module

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,18 @@
 use pyo3::prelude::*;
 
-#[pyfunction]
-fn hello_from_bin() -> String {
-    "Hello from endrs!".to_string()
-}
+mod graph;
+mod incremental;
+mod inference;
+mod item_cf;
+mod ordering;
+mod serialization;
+mod similarities;
+mod sparse;
+mod swing;
+mod user_cf;
+mod utils;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
@@ -11,6 +20,18 @@ fn hello_from_bin() -> String {
 #[pymodule]
 #[pyo3(name = "endrs_ext")]
 fn endrs(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(hello_from_bin, m)?)?;
+    m.add_class::<swing::PySwing>()?;
+    m.add_function(wrap_pyfunction!(swing::save, m)?)?;
+    m.add_function(wrap_pyfunction!(swing::load, m)?)?;
+
+    m.add_class::<user_cf::PyUserCF>()?;
+    m.add_function(wrap_pyfunction!(user_cf::save, m)?)?;
+    m.add_function(wrap_pyfunction!(user_cf::load, m)?)?;
+
+    m.add_class::<item_cf::PyItemCF>()?;
+    m.add_function(wrap_pyfunction!(item_cf::save, m)?)?;
+    m.add_function(wrap_pyfunction!(item_cf::load, m)?)?;
+
+    m.add("__version__", VERSION)?;
     Ok(())
 }
