8f3f91f71203190a3b3988d4f98d816493797d30
massquantity <wdmjjxg@163.com>
2025-12-23 20:44:03 +0800

    refactor(rust): improve OOV handling in CSR matrix merging

--- rust/src/sparse.rs
+++ rust/src/sparse.rs
@@ -26,10 +26,9 @@ impl<T: Copy + Eq + Hash + Ord, U: Copy> CsrMatrix<T, U> {
         self.indptr.len().saturating_sub(1)
     }
 
-    fn to_dok(&self, n_rows: Option<usize>) -> DokMatrix<T, U> {
+    fn to_dok(&self, n_rows: usize) -> DokMatrix<T, U> {
         let mut data = Vec::new();
-        let n_rows = n_rows.unwrap_or_else(|| self.n_rows());
-        for i in 0..n_rows {
+        for i in 0..=n_rows {
             if let Some(row) = get_row(self, i, false) {
                 data.push(FxHashMap::from_iter(row))
             } else {
@@ -42,7 +41,7 @@ impl<T: Copy + Eq + Hash + Ord, U: Copy> CsrMatrix<T, U> {
     pub fn merge(
         this: &CsrMatrix<T, U>,
         other: &CsrMatrix<T, U>,
-        n_rows: Option<usize>,
+        n_rows: usize,
     ) -> CsrMatrix<T, U> {
         let mut dok_matrix = this.to_dok(n_rows);
         dok_matrix.merge(other).to_csr()
@@ -125,7 +124,7 @@ where
     U: Copy,
 {
     fn merge(&mut self, other: &CsrMatrix<T, U>) -> &Self {
-        for (i, row) in other.iter().enumerate() {
+        for (i, row) in other.iter().enumerate().skip(1) {
             if row.is_empty() {
                 continue;
             }
@@ -164,78 +163,108 @@ where
 mod tests {
     use super::*;
 
+    /// Verifies CSR matrix maintains OOV layout: row 0 empty, no column 0 entries.
+    fn assert_oov_layout(matrix: &CsrMatrix<i32, i32>, n_real_rows: usize) {
+        assert_eq!(matrix.indptr[0], 0, "OOV row must stay empty");
+        assert_eq!(
+            matrix.indptr.len(),
+            n_real_rows + 2,
+            "indptr length should be OOV row + real rows + trailing pointer"
+        );
+        assert!(
+            matrix.indices.iter().all(|&col| col != 0),
+            "column 0 is reserved for OOV"
+        );
+    }
+
     #[test]
     fn test_add_sparse_matrix() {
-        // [[1, 0, 0], [0, 0, 1]]
+        // Base (5 rows: OOV + users 1-4; 4 cols: OOV + items 1-3)
+        // [
+        //   [],           // row 0: OOV
+        //   [0,2,0,0],    // row 1
+        //   [0,0,0,3],    // row 2
+        //   [0,0,0,0],    // row 3
+        //   [0,0,0,0],    // row 4
+        // ]
         let mut matrix = CsrMatrix {
-            indices: vec![0, 2],
-            indptr: vec![0, 1, 2],
-            data: vec![1, 1],
+            indices: vec![1, 3],
+            indptr: vec![0, 0, 1, 2, 2, 2],
+            data: vec![2, 3],
         };
-        // [[0, 0, 0], [1, 0, 2], [3, 3, 0]]
-        let matrix_large = CsrMatrix {
-            indices: vec![0, 2, 0, 1],
-            indptr: vec![0, 0, 2, 4],
-            data: vec![1, 2, 3, 3],
-        };
-        // [[2, 0, 4]]
-        let matrix_small = CsrMatrix {
-            indices: vec![0, 2],
-            indptr: vec![0, 2],
-            data: vec![2, 4],
+        // Incoming (same shape; overwrites col 1 in row 1, extends rows 3/4)
+        // [
+        //   [],              // row 0: OOV
+        //   [0,1,0,0],       // row 1
+        //   [],              // row 2
+        //   [0,0,5,0],       // row 3
+        //   [0,4,0,1],       // row 4
+        // ]
+        let matrix_new = CsrMatrix {
+            indices: vec![1, 2, 1, 3],
+            indptr: vec![0, 0, 1, 1, 2, 4],
+            data: vec![1, 5, 4, 1],
         };
 
-        // [[1, 0, 0], [1, 0, 2], [3, 3, 0]]
-        matrix = CsrMatrix::merge(&matrix, &matrix_large, Some(3));
-        assert_eq!(matrix.indices, vec![0, 0, 2, 0, 1]);
-        assert_eq!(matrix.indptr, vec![0, 1, 3, 5]);
-        assert_eq!(matrix.data, vec![1, 1, 2, 3, 3]);
-
-        // [[2, 0, 4], [1, 0, 2], [3, 3, 0]]
-        matrix = CsrMatrix::merge(&matrix, &matrix_small, Some(3));
-        assert_eq!(matrix.indices, vec![0, 2, 0, 2, 0, 1]);
-        assert_eq!(matrix.indptr, vec![0, 2, 4, 6]);
-        assert_eq!(matrix.data, vec![2, 4, 1, 2, 3, 3]);
+        matrix = CsrMatrix::merge(&matrix, &matrix_new, 4);
+        assert_eq!(matrix.indptr, vec![0, 0, 1, 2, 3, 5]);
+        assert_eq!(matrix.indices, vec![1, 3, 2, 1, 3]);
+        assert_eq!(matrix.data, vec![1, 3, 5, 4, 1]);
+        assert_oov_layout(&matrix, 4);
     }
 
     #[test]
     #[should_panic(expected = "index out of bounds: the len is 2 but the index is 2")]
     fn test_add_insufficient_size() {
-        let new_size = 2;
-        // [[1, 0, 0], [0, 0, 1]]
+        // Only OOV + row 1 available
         let matrix = CsrMatrix {
-            indices: vec![0, 2],
-            indptr: vec![0, 1, 2],
-            data: vec![1, 1],
-        };
-        // [[0, 0, 0], [1, 0, 2], [3, 3, 0]]
-        let matrix_large = CsrMatrix {
-            indices: vec![0, 2, 0, 1],
-            indptr: vec![0, 0, 2, 4],
-            data: vec![1, 2, 3, 3],
-        };
-        CsrMatrix::merge(&matrix, &matrix_large, Some(new_size));
-    }
-
-    #[test]
-    fn test_add_with_empty_rows() {
-        // [[0, 0, 0], [0, 0, 1]]
-        let mut matrix = CsrMatrix {
-            indices: vec![2],
+            indices: vec![1],
             indptr: vec![0, 0, 1],
             data: vec![1],
         };
-        // [[0, 0, 0], [1, 0, 2], [3, 3, 0]]
+        // Incoming has OOV + rows 1-2; n_rows=1 is too small
         let matrix_large = CsrMatrix {
-            indices: vec![0, 2, 0, 1],
-            indptr: vec![0, 0, 2, 4],
-            data: vec![1, 2, 3, 3],
+            indices: vec![1, 2],
+            indptr: vec![0, 0, 1, 2],
+            data: vec![1, 1],
         };
 
-        // [[0, 0, 0], [1, 0, 2], [3, 3, 0]]
-        matrix = CsrMatrix::merge(&matrix, &matrix_large, Some(3));
-        assert_eq!(matrix.indices, vec![0, 2, 0, 1]);
-        assert_eq!(matrix.indptr, vec![0, 0, 2, 4]);
-        assert_eq!(matrix.data, vec![1, 2, 3, 3]);
+        CsrMatrix::merge(&matrix, &matrix_large, 1);
+    }
+
+    #[test]
+    fn test_merge_with_larger_matrix() {
+        // OOV + rows 1-3, only row 2 populated
+        // [
+        //   [],        // OOV
+        //   [],        // row 1
+        //   [0,0,0,1], // row 2
+        //   [],        // row 3
+        // ]
+        let mut matrix = CsrMatrix {
+            indices: vec![3],
+            indptr: vec![0, 0, 0, 1, 1],
+            data: vec![1],
+        };
+        // Larger matrix with OOV + rows 1-5
+        // [
+        //   [],           // OOV
+        //   [0,0,2,0],    // row 1
+        //   [],           // row 2
+        //   [0,3,0,0],    // row 3
+        //   [0,0,0,4],    // row 4 (new)
+        //   [0,5,0,0],    // row 5 (new)
+        // ]
+        let matrix_new = CsrMatrix {
+            indices: vec![2, 1, 3, 1],
+            indptr: vec![0, 0, 1, 1, 2, 3, 4],
+            data: vec![2, 3, 4, 5],
+        };
+
+        matrix = CsrMatrix::merge(&matrix, &matrix_new, 5);
+        assert_eq!(matrix.indptr, vec![0, 0, 1, 2, 3, 4, 5]);
+        assert_eq!(matrix.indices, vec![2, 3, 1, 3, 1]);
+        assert_eq!(matrix.data, vec![2, 1, 3, 4, 5]);
+        assert_oov_layout(&matrix, 5);
     }
 }
